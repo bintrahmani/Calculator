ddd93964a18c4ae5050d8c66f2a0a3570712fc15
bintrahmani <dhinilistiyanii2019@gmail.com>
2024-10-13 13:41:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.slice(0, -1);
+}
+
+function calculate() {
+    let expression = document.getElementById('display').value;
+    if (expression) {
+        document.getElementById('display').value = eval(expression);
+    }
+}
